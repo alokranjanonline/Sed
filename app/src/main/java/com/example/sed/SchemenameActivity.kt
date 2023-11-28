961af5f7296ea9b5f3949f702537f4bd9aba25bc
alokranjanonline <alokranjan999@gmail.com>
2023-11-28 21:08:03 +0530

    1

--- app/src/main/java/com/example/sed/SchemenameActivity.kt
+++ app/src/main/java/com/example/sed/SchemenameActivity.kt
@@ -1,12 +1,18 @@
 package com.example.sed
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.KeyEvent
+import android.widget.TextView
 
 class SchemenameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_schemename)
+        var receiver_msg: TextView = findViewById(R.id.txtSchemename)
+        val intet: Intent
+        val str = intent.getStringExtra("message_key")
+        receiver_msg.text=str
     }
 }
